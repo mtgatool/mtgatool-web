ca926d1caeac1713465da22d007b11d801f851cf
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2019-12-17 00:12:03 -0300

    dfc cards hover

--- src/components/card-hover/index.jsx
+++ src/components/card-hover/index.jsx
@@ -4,7 +4,7 @@ import css from "./cardhover.css";
 import db from "../../shared/database";
 import NotFound from "../../cssimages/notfound.png";
 import { useWebContext } from "../../web-provider";
-
+import { FACE_DFC_BACK, FACE_DFC_FRONT } from "../../shared/constants";
 function CardHover() {
   const webContext = useWebContext();
 
@@ -23,8 +23,36 @@ function CardHover() {
     };
   };
 
+  const getStyleDfc = ctx => {
+    let cardObj = db.card(ctx.HoverGrpId);
+    let newImg = `url(${NotFound})`;
+    let opacity = ctx.HoverOpacity;
+    if (
+      cardObj &&
+      (cardObj.dfc == FACE_DFC_BACK || cardObj.dfc == FACE_DFC_FRONT) &&
+      cardObj.dfcId
+    ) {
+      cardObj = db.card(cardObj.dfcId);
+      try {
+        newImg = `url(https://img.scryfall.com/cards${cardObj.images.normal}`;
+      } catch (e) {
+        newImg = `url(${NotFound})`;
+      }
+    } else {
+      opacity = 0;
+    }
+
+    return {
+      opacity: opacity,
+      backgroundImage: newImg
+    };
+  };
+
   return (
-    <div style={getStyle(webContext)} className={css["card-hover-main"]} />
+    <>
+      <div style={getStyleDfc(webContext)} className={css["card-hover-dfc"]} />
+      <div style={getStyle(webContext)} className={css["card-hover-main"]} />
+    </>
   );
 }
 
