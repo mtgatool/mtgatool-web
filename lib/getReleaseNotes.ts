24733bdcdfa902d9a17b58bd10c31c7a16655f9c
Manwe <manuel.etchegaray7@gmail.com>
2022-09-19 22:35:50 -0300

    fix release notes

--- lib/getReleaseNotes.ts
+++ lib/getReleaseNotes.ts
@@ -1,10 +1,15 @@
 import fs from "fs";
 import { join } from "path";
 
-const resourcesDir = join(process.cwd(), "resources");
-
 export function getReleaseNotes() {
-  const fullPath = join(resourcesDir, `releasenotes.txt`);
+  const fullPath = join(
+    __dirname,
+    "..",
+    "..",
+    "..",
+    "resources",
+    `releasenotes.txt`
+  );
   const fileContents = fs.readFileSync(fullPath, "utf8");
 
   return fileContents;
